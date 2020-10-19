1468159594ef3f728ab453e02d27d455499c1502
Remizovatonya <Remizovatonya@mail.com>
2020-10-20 01:55:49 +0300

    Первые изменения

--- Matrix/Main.cpp
+++ Matrix/Main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include "Matrix.h"
+#include "MyVector.h"
 
 int main()
 {
+	Vector <int> a(3, 3);
+	TMatrix <int> A(3);
+	TMatrix <int> B = A;
+	TMatrix <int> C(3);
+	A + B;
 	return 0;
+
 }
--- test/test_Matrix.cpp
+++ test/test_Matrix.cpp
@@ -1,15 +1,12 @@
 
-
+#include "Matrix.h"
 #include <gtest.h>
 
-//TEST(TSet, can_get_max_power_set)
-//{
-//  const int size = 5;
-//  TSet set(size);
-//
-//  EXPECT_EQ(size, set.GetMaxPower());
-//}
-//
+TEST(TMatrix, can_create_matrix)
+{
+    ASSERT_NO_THROW(TMatrix<int> A(3));
+}
+
 //TEST(TSet, can_insert_non_existing_element)
 //{
 //  const int size = 5, k = 3;
